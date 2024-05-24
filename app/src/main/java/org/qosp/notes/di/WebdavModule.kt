2a6686b0f4a2f37bd8b04d8e501e31b81de4c0c0
tangshg <tangshg@yeah.net>
2024-05-24 09:50:57 +0800

    目前位置，nextCloud 连接并未受到影响

--- app/src/main/java/org/qosp/notes/di/WebdavModule.kt
+++ app/src/main/java/org/qosp/notes/di/WebdavModule.kt
@@ -1,4 +1,44 @@
 package org.qosp.notes.di
 
-class WebdavModule {
+import android.app.Application
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import org.qosp.notes.App
+import org.qosp.notes.data.repo.IdMappingRepository
+import org.qosp.notes.data.repo.NoteRepository
+import org.qosp.notes.data.repo.NotebookRepository
+import org.qosp.notes.data.sync.nextcloud.NextcloudAPI
+import org.qosp.notes.data.sync.nextcloud.NextcloudManager
+import org.qosp.notes.data.sync.webdav.WebdavAPI
+import org.qosp.notes.data.sync.webdav.WebdavManager
+import org.qosp.notes.ui.utils.ConnectionManager
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WebdavModule {
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    //dagger 模块的使用，这里提供一个 webdav
+
+
+
+    @Provides
+    @Singleton
+    fun provideWebdavManager(
+        webdavAPI: WebdavAPI,
+        @Named(NO_SYNC) noteRepository: NoteRepository,
+        @Named(NO_SYNC) notebookRepository: NotebookRepository,
+        idMappingRepository: IdMappingRepository,
+    ) = WebdavManager(webdavAPI, noteRepository, notebookRepository, idMappingRepository)
 }
